72d15600f4757ffe2ff2bb057cfc9536cdc0b733
faizbyp <faizbyp@gmail.com>
2023-03-08 17:26:37 +0700

    useEffect intro

--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Blog from '../components/Blog';
 
 const BlogPosts = () => {
@@ -8,17 +8,27 @@ const BlogPosts = () => {
     {title: 'anjay3', body: 'buuuud', id: 3}
   ]);
 
+  const [name, setName] = useState('pais')
+
   const handleHide = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  // fungsi yg dijalanin tiap kali render
+  useEffect(() => {
+    console.log('use effect coy')
+    console.log(name)
+  }, [name]) // array dependency buat nentuin kapan fungsi dijalanin (saat suatu state berubah)
+
   return (
     <Fragment>
       <h1>Blog Posts Archive</h1>
       <Blog blogs={blogs} handleHide={handleHide} />
+      <button onClick={() => setName('udin')} style={{marginTop: "20px", backgroundColor: "green"}}>ganti nama</button>
+      <p>{name}</p>
       <h1>Blog yang id-nya di atas 1</h1>
-      <Blog blogs={blogs.filter((blog) => blog.id > 1)} />
+      <Blog blogs={blogs.filter((blog) => blog.id > 1)} handleHide={handleHide} />
     </Fragment>
   )
 }
