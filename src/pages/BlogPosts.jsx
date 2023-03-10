fce98ef04de1d06bfaafaac5e6353d6c42eeb503
faizbyp <faizbyp@gmail.com>
2023-03-10 18:33:35 +0700

    add loading message

--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -3,8 +3,8 @@ import Blog from '../components/Blog'
 
 const BlogPosts = () => {
   const [blogs, setBlogs] = useState(null)
-
   const [name, setName] = useState('pais')
+  const [isPending, setIsPending] = useState(true)
 
   // const handleHide = (id) => {
   //   const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -13,20 +13,24 @@ const BlogPosts = () => {
 
   // fungsi yg dijalanin tiap kali render
   useEffect(() => {
-    console.log('use effect coy')
-    fetch("http://localhost:8000/blogs")
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      console.log('use effect coy')
+      fetch("http://localhost:8000/blogs")
+        .then(res => {
+          return res.json()
+        })
+        .then(data => {
+          setBlogs(data)
+          setIsPending(false)
+        })
+    }, 2000)
   }, []) // array dependency buat nentuin kapan fungsi dijalanin (saat suatu state berubah)
 
   return (
     <Fragment>
       <h1>Blog Posts Archive</h1>
-      {blogs && <Blog blogs={blogs} />}
+      {isPending && <h2>Loading...</h2>}
+      {blogs && <Blog blogs={blogs} />} {/* conditional rendering ketika true, elemen dirender */}
       <button onClick={() => setName('udin')} style={{marginTop: "20px", backgroundColor: "green"}}>ganti nama</button>
       <p>{name}</p>
     </Fragment>
